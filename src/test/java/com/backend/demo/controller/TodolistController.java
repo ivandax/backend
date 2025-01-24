653a4667cdae842cc8ba4a05085092ddcad4a4d5
ivandax <ivandax_89@hotmail.com>
2025-01-24 07:02:44 +0100

    Add initial tests for todolist controllre

---
+++ src/test/java/com/backend/demo/controller/TodolistController.java
@@ -0,0 +1,133 @@
+package com.backend.demo.controller;
+
+import com.backend.demo.model.Permission;
+import com.backend.demo.model.Role;
+import com.backend.demo.model.User;
+import com.backend.demo.repository.*;
+import com.backend.demo.service.mailing.EmailService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class TodolistController {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmailService emailService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Autowired
+    private UserVerificationTokenRepository userVerificationTokenRepository;
+
+    @Autowired
+    private InvalidTokenRepository invalidTokenRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @Autowired
+    private TodolistRepository todolistRepository;
+
+    @BeforeEach
+    void setup() {
+        invalidTokenRepository.deleteAll();
+        userVerificationTokenRepository.deleteAll();
+        userRepository.deleteAll();
+        roleRepository.deleteAll();
+        permissionRepository.deleteAll();
+        organizationRepository.deleteAll();
+        todolistRepository.deleteAll();
+
+        Permission permissionReadUsers = new Permission("read:users");
+        permissionRepository.saveAll(List.of(permissionReadUsers));
+
+        Role dev = new Role("DEV");
+        Role admin = new Role("ADMIN");
+        admin.setPermissions(new HashSet<>(List.of(permissionReadUsers)));
+        roleRepository.saveAll(List.of(dev, admin));
+
+        User adminUser = new User();
+        adminUser.addRole(admin);
+        adminUser.setUsername("admin@mail.com");
+        adminUser.setPassword("testPassword");
+        adminUser.setVerified(true);
+        userRepository.save(adminUser);
+
+        User noRoleUser = new User();
+        noRoleUser.setUsername("no_permissions@mail.com");
+        noRoleUser.setPassword("testPassword");
+        noRoleUser.setVerified(true);
+        userRepository.save(noRoleUser);
+    }
+
+    @Test
+    @DisplayName("Failure: Create todolists without auth")
+    public void createTodolistsFailureNoAuth() throws Exception {
+        mockMvc.perform(post("/api/todolists")).andExpect(status().isForbidden()).andReturn();
+    }
+
+    @Test
+    @DisplayName("Failure: Create todolists without create:todolist permission")
+    public void createTodolistsFailureNoPermissions() throws Exception {
+        MvcResult loginResult = mockMvc.perform(post("/login")
+                        .contentType(MediaType
+                                .APPLICATION_FORM_URLENCODED_VALUE)
+                        .param("username", "no_permissions@mail.com")
+                        .param("password", "testPassword"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> tokensResponse =
+                objectMapper.readValue(loginResult.getResponse().getContentAsString(), Map.class);
+        String accessToken = tokensResponse.get("access_token");
+
+        mockMvc.perform(post("/api/todolists")
+                        .header("authorization", "Bearer " + accessToken))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("Failure: Get todolists without auth")
+    public void getUsersFailureNoAuth() throws Exception {
+        mockMvc.perform(get("/api/todolists")).andExpect(status().isForbidden()).andReturn();
+    }
+}
