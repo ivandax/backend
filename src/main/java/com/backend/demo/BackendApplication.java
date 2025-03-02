2a367ac4a7ddd90dfb5ddb028e6f1c1015afaba7
ivandax <ivandax_89@hotmail.com>
2025-03-02 10:30:13 +0100

    Add log of active profile

--- src/main/java/com/backend/demo/BackendApplication.java
+++ src/main/java/com/backend/demo/BackendApplication.java
@@ -1,15 +1,23 @@
 package com.backend.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.backend.demo.config")
 public class BackendApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		ConfigurableApplicationContext context = app.run(args);
+		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+		logger.info("Active profiles: {}", Arrays.toString(activeProfiles));
 	}
 
 }
