78667c0db583b09d4a095d47b271eba2b33113aa
ivandax <ivandax_89@hotmail.com>
2025-03-05 08:21:24 +0100

    Add more explicit logging

--- src/main/java/com/backend/demo/BackendApplication.java
+++ src/main/java/com/backend/demo/BackendApplication.java
@@ -1,23 +1,19 @@
 package com.backend.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.backend.demo.config")
 public class BackendApplication {
-	private static final Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BackendApplication.class);
-		ConfigurableApplicationContext context = app.run(args);
-		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
-		logger.info("Active profiles: {}", Arrays.toString(activeProfiles));
+		Environment env = app.run(args).getEnvironment();
+		System.out.println("My active profiles: " + Arrays.toString(env.getActiveProfiles()));
 	}
 
 }
